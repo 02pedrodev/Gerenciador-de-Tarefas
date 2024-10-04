3626f96915d30144f7a0575fa55996e93c27419c
Pedro Igor <pedrosousa058@gmail.com>
2024-10-04 12:22:08 -0300

    adicionando função de deletar tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,41 @@ function App() {
       id: 1,
       title: "Estudar Programação",
       descripition: "Estudar sobre React",
-      completed: false,
+      Iscompleted: false,
     },
     {
       id: 2,
       title: "Estudar Inglês",
       descripition: "Estudar sobre verbos",
-      completed: false,
+      Iscompleted: false,
     },
     {
       id: 3,
       title: "Estudar Matematica",
       descripition: "Estudar sobre Lógica de Programação",
-      completed: false,
+      Iscompleted: false,
     },
   ]);
+  // //  Recebeu taskId para identificar qual tarefa foi clicada
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
       // PRECISO ATUALIZAR ESSA TAREFA
       if (task.id === taskId) {
-        return { ...task, completed: !task.completed };
+        return { ...task, Iscompleted: !task.Iscompleted };
       }
       // // Não preciso atualizar essa tarefa
       return task;
     });
     setTasks(newTasks);
+  }
+  function onClickDelete(taskId) {
+    const deleteTasks = tasks.filter((task) => {
+      if (task.id === taskId) {
+        return;
+      }
+      return task;
+    });
+    setTasks(deleteTasks);
   }
 
   return (
@@ -41,7 +51,11 @@ function App() {
         <h1 className="text-slate-100 text-3xl font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onClickDelete={onClickDelete}
+        />
         <AddTask />
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks(props) {
   return (
@@ -8,12 +8,23 @@ function Tasks(props) {
       {props.tasks.map((task) => (
         // quando vc quer utilizar uma lista e cada item vai renderizar um elemento voce precisa botar uma key na sua li, div, p  ou o que seja.
         <li key={task.id} className="flex gap-2">
-          <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={` bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.Iscompleted && "line-through"
+            }`}
+          >
             {" "}
             {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => props.onClickDelete(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <TrashIcon />
           </button>
         </li>
       ))}
